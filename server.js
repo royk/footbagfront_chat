ae7ec503b925a0b861b4337310826ba2695dd859
Roy <roeiklein@gmail.com>
2013-04-07 08:13:17 +0300

    added history to chat

--- server.js
+++ server.js
@@ -2,13 +2,26 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
+var history = [];
 
 server.listen(8080);
 
 app.use(express.static(__dirname + '/public'));
 
 io.sockets.on('connection', function (socket) {
-  socket.on('msg', function (data) {
-    io.sockets.emit('new', data);
-  });
+	socket.on('history', function() {
+		if (history.length>0) {
+			var msg = JSON.stringify(history);
+			io.sockets.emit('new', msg);
+		}
+
+	}),
+	socket.on('msg', function (data) {
+	  	history.push(data);
+	  	if (history.length>50) {
+	  		history.shift();
+	  	}
+	  	var msg = JSON.stringify([data]);
+		io.sockets.emit('new', msg);
+	});
 });
