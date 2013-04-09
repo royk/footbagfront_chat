741b27da84c71f72dcf24aebf16f31e708b8ae11
Roy <roeiklein@gmail.com>
2013-04-09 23:08:15 +0300

    Read and write chat history in a file. Sanitize and limit incoming data.

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 var express = require('express');
+var sanitize = require('validator').sanitize;
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 var history = [];
+var max_message_length = 250;
+var max_message_history = 50;
+var fs = require('fs');
+
+// read chat history from file
+fs.readFile("chat.log", "utf8", function(err, data) {
+	if (err) {
+		return console.log(err);
+	}
+	history = JSON.parse(unescape(data));
+});
+
+function writeHistoryToFile() {
+	if (history.length>0) {
+		fs.writeFile("chat.log", escape(JSON.stringify(history)));
+	}
+}
 
 server.listen(8080);
 
@@ -14,11 +32,20 @@ io.sockets.on('connection', function (socket) {
 		io.sockets.emit('history', msg);
 	}),
 	socket.on('msg', function (data) {
-	  	history.push(data);
-	  	if (history.length>50) {
-	  		history.shift();
-	  	}
-	  	var msg = JSON.stringify([data]);
-		io.sockets.emit('new', msg);
+		// sanitize data
+		if (data) {
+			data.msg = sanitize(data.msg).entityEncode();
+			if (data.msg.length > max_message_length) {
+				data.msg = data.msg.substring(0, max_message_length);
+			}
+			history.push(data);
+			while (history.length>max_message_history) {
+				history.shift();
+			}
+			writeHistoryToFile();
+			var msg = JSON.stringify([data]);
+			console.log(data,msg);
+			io.sockets.emit('new', msg);
+		}
 	});
 });
